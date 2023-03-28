64a422f4bd5ddec8484c743ff1b37a47f5cec3fd
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-03-28 21:56:19 +0400

    add: theme provider
    This commit adds a theme provider to customize theme colors and typography for MUI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+import { useMemo } from 'react';
+import { themeSettings } from '@/theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
