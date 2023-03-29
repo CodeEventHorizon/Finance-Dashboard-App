1f0f2a5e89ab0ea078135c38dedcd269c8c2c597
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-03-29 18:00:08 +0400

    add: routes using react-router-dom and Box component from MUI
    This commit adds routes using react-router-dom to the App.tsx and uses a Box component from MUI to style the whole page width, height and padding

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,29 @@
 import { createTheme } from '@mui/material/styles';
 import { useMemo } from 'react';
 import { themeSettings } from '@/theme';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Box, CssBaseline, ThemeProvider } from '@mui/material';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from "@/scenes/navbar";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>Dashboard page</div>} />
+              <Route
+                path="/predictions"
+                element={<div>Predictions page</div>}
+              />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
