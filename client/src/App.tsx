67f0923aef84c898214abc7c0893a5f71fb185c5
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-03-30 19:59:30 +0400

    add: grid layout for dashboard
    This commit adds a responsive grid layout for dashboard that is using useMediaQuery from MUI, where min-width is 1200px and depending on the width of the screen it changes the position of each box from a to j

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { themeSettings } from '@/theme';
 import { Box, CssBaseline, ThemeProvider } from '@mui/material';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from "@/scenes/navbar";
+import Dashboard from "@/scenes/dashboard";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
@@ -15,7 +16,7 @@ function App() {
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
             <Navbar />
             <Routes>
-              <Route path="/" element={<div>Dashboard page</div>} />
+              <Route path="/" element={<Dashboard />} />
               <Route
                 path="/predictions"
                 element={<div>Predictions page</div>}
