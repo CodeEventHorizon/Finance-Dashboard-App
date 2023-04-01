4600c74f15d0838e85e1b72ddf33365d5c8304e9
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-04-01 21:31:29 +0400

    add: index.js clean setup
    This commit adds an index.js which uses some third party middleware, like cors, helmet, body-parser, morgan, then starts up a server on port 4000, and the connects to a MongoDB database using mongoose

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+/* CONFIGURATIONS */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, async () => {
+  console.log(`Server listening on: ${PORT}`);
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Mongoose connected successfully');
+  } catch (error) {
+    console.error(error);
+  }
+});
