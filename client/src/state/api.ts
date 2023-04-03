63c2cfce839634b0f3e151c7b61959dc853d22b2
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-04-03 23:24:49 +0400

    update: redux api, that fetches data from the server according to types provided in state/type.ts

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,11 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { GetKpisResponse } from './types';
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
   reducerPath: 'main',
   tagTypes: ['Kpis'],
   endpoints: (build) => ({
-    getKpi: build.query<void, void>({
+    getKpi: build.query<Array<GetKpisResponse>, void>({
       query: () => 'kpi/kpis/',
       providesTags: ['Kpis'],
     }),
---
+++ client/src/state/types.ts
@@ -0,0 +1,36 @@
+export interface ExpensesByCategory {
+  salaries: string;
+  supplies: string;
+  services: string;
+}
+
+export interface Month {
+  id: string;
+  month: string;
+  revenue: string;
+  expenses: string;
+  nonOperationalExpenses: string;
+  operationalExpenses: string;
+}
+
+export interface Day {
+  id: string;
+  date: string;
+  revenue: string;
+  expenses: string;
+}
+
+
+export interface GetKpisResponse {
+  id: string;
+  _id: string;
+  __v: string;
+  totalProfit: string;
+  totalRevenue: string;
+  totalExpenses: string;
+  expensesByCategory: ExpensesByCategory;
+  monthlyData: Array<Month>;
+  dailyData: Array<Day>;
+  createdAt: string;
+  updatedAt: string;
+}
