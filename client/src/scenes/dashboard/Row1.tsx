ad419a61ed3f8e121eae2109c424ca00c522acbb
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-04-03 23:17:40 +0400

    add: Row1 component
    This commit adds Row1 with 3 DashboardBox components that use recharts library and fetch data from the server using redux useGetKpiQuery() from state/api.ts

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,13 +1,248 @@
+import BoxHeader from '@/components/BoxHeader';
 import DashboardBox from '@/components/DashboardBox';
 import { useGetKpiQuery } from '@/state/api';
+import { useTheme } from '@mui/material';
+import { useMemo } from 'react';
+import {
+  Area,
+  AreaChart,
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
 
 const Row1 = () => {
+  const { palette } = useTheme();
   const { data } = useGetKpiQuery();
+
+  const revenue = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ month, revenue }) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: parseFloat(revenue.replace('$', '').replace(',', '')),
+        };
+      })
+    );
+  }, [data]);
+
+  const revenueExpenses = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ month, revenue, expenses }) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: parseFloat(revenue.replace('$', '').replace(',', '')),
+          expenses: parseFloat(expenses.replace('$', '').replace(',', '')),
+        };
+      })
+    );
+  }, [data]);
+
+  const revenueProfit = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ month, revenue, expenses }) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: parseFloat(revenue.replace('$', '').replace(',', '')),
+          profit: (
+            parseFloat(revenue.replace('$', '').replace(',', '')) -
+            parseFloat(expenses.replace('$', '').replace(',', ''))
+          ).toFixed(2),
+        };
+      })
+    );
+  }, [data]);
+
+  // console.log('🚀 ~ file: Row1.tsx:20 ~ revenue ~ revenue:', revenue);
+  // console.log(
+  //   '🚀 ~ file: Row1.tsx:32 ~ revenueExpenses ~ revenueExpenses:',
+  //   revenueExpenses
+  // );
+  // console.log(
+  //   '🚀 ~ file: Row1.tsx:45 ~ revenueProfit ~ revenueProfit:',
+  //   revenueProfit
+  // );
   return (
     <>
-      <DashboardBox gridArea="a"></DashboardBox>
-      <DashboardBox gridArea="b"></DashboardBox>
-      <DashboardBox gridArea="c"></DashboardBox>
+      <DashboardBox gridArea="a">
+        <BoxHeader
+          title="Revenue and Expenses"
+          subtitle="top line represents revenue, bottom line represents expenses"
+          sideText="+4%"
+        />
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={500}
+            height={400}
+            data={revenueExpenses}
+            margin={{ top: 15, right: 25, left: -10, bottom: 60 }}>
+            <defs>
+              <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0.5}
+                />
+                <stop
+                  offset="95%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0}
+                />
+              </linearGradient>
+              <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0.5}
+                />
+                <stop
+                  offset="95%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0}
+                />
+              </linearGradient>
+            </defs>
+            <XAxis
+              dataKey="name"
+              tickLine={false}
+              style={{
+                fontSize: '10px',
+              }}
+            />
+            <YAxis
+              tickLine={false}
+              axisLine={{ strokeWidth: '0' }}
+              domain={[8000, 23000]}
+              style={{
+                fontSize: '10px',
+              }}
+            />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="revenue"
+              stroke={palette.primary.main}
+              fillOpacity={1}
+              fill="url(#colorRevenue)"
+            />
+            <Area
+              type="monotone"
+              dataKey="expenses"
+              stroke={palette.primary.main}
+              fillOpacity={1}
+              fill="url(#colorExpenses)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </DashboardBox>
+      <DashboardBox gridArea="b">
+        <BoxHeader
+          title="Profit and Revenue"
+          subtitle="top line represents revenue, bottom line represents expenses"
+          sideText="+4%"
+        />
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            data={revenueProfit}
+            margin={{ top: 20, right: 0, left: -10, bottom: 55 }}>
+            <CartesianGrid vertical={false} stroke={palette.grey[800]} />
+            <XAxis
+              dataKey="name"
+              tickLine={false}
+              style={{
+                fontSize: '10px',
+              }}
+            />
+            <YAxis
+              yAxisId="left"
+              tickLine={false}
+              axisLine={false}
+              style={{
+                fontSize: '10px',
+              }}
+            />
+            <YAxis
+              yAxisId="right"
+              orientation="right"
+              tickLine={false}
+              axisLine={false}
+              style={{
+                fontSize: '10px',
+              }}
+            />
+            <Tooltip />
+            <Legend
+              height={20}
+              wrapperStyle={{
+                margin: '0 0 10px 0',
+              }}
+            />
+            <Line
+              yAxisId="left"
+              type="monotone"
+              dataKey="profit"
+              stroke={palette.tertiary[500]}
+            />
+            <Line
+              yAxisId="right"
+              type="monotone"
+              dataKey="revenue"
+              stroke={palette.primary.main}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </DashboardBox>
+      <DashboardBox gridArea="c">
+        <BoxHeader
+          title="Revenue Month by Month"
+          subtitle="graph representing the revenue month by month"
+          sideText="+4%"
+        />
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={500}
+            height={300}
+            data={revenue}
+            margin={{ top: 17, right: 15, left: -5, bottom: 58 }}>
+            <defs>
+              <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+                <stop
+                  offset="5%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0.8}
+                />
+                <stop
+                  offset="95%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0}
+                />
+              </linearGradient>
+            </defs>
+            <CartesianGrid vertical={false} stroke={palette.grey[800]} />
+            <XAxis
+              dataKey="name"
+              tickLine={false}
+              style={{ fontSize: '10px' }}
+            />
+            <YAxis
+              tickLine={false}
+              style={{ fontSize: '10px' }}
+            />
+            <Tooltip />
+            <Bar dataKey="revenue" fill="url(#colorRevenue)" />
+          </BarChart>
+        </ResponsiveContainer>
+      </DashboardBox>
     </>
   );
 };
