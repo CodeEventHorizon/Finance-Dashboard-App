de5df8cc4d227791a6e2bc71feca491276e06d9c
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-04-11 03:18:50 +0400

    add: Row3 component mui data grid tables

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -1,10 +1,132 @@
+import BoxHeader from '@/components/BoxHeader';
 import DashboardBox from '@/components/DashboardBox';
+import {
+  useGetKpiQuery,
+  useGetProductsQuery,
+  useGetTransactionsQuery,
+} from '@/state/api';
+import { DataGrid, GridCellParams } from '@mui/x-data-grid';
+import { Box, useTheme } from '@mui/material';
 
 const Row3 = () => {
+  const { palette } = useTheme();
+  const { data: kpiData } = useGetKpiQuery();
+  const { data: productData } = useGetProductsQuery();
+  const { data: transactionsData } = useGetTransactionsQuery();
+
+  const productColumns = [
+    {
+      field: '_id',
+      headerName: 'id',
+      flex: 1,
+    },
+    {
+      field: 'expense',
+      headerName: 'Expense',
+      flex: 0.5,
+      renderCell: (params: GridCellParams) => `${params.value}`,
+    },
+    {
+      field: 'price',
+      headerName: 'Price',
+      flex: 0.5,
+      renderCell: (params: GridCellParams) => `${params.value}`,
+    },
+  ];
+
+  const transactionsColumns = [
+    {
+      field: '_id',
+      headerName: 'id',
+      flex: 1,
+    },
+    {
+      field: 'buyer',
+      headerName: 'Buyer',
+      flex: 0.67,
+    },
+    {
+      field: 'amount',
+      headerName: 'Amount',
+      flex: 0.35,
+      renderCell: (params: GridCellParams) => `${params.value}`,
+    },
+    {
+      field: 'productIds',
+      headerName: 'Count',
+      flex: 0.1,
+      renderCell: (params: GridCellParams) => (params.value as string).length,
+    },
+  ];
+
   return (
     <>
-      <DashboardBox gridArea="g"></DashboardBox>
-      <DashboardBox gridArea="h"></DashboardBox>
+      <DashboardBox gridArea="g">
+        <BoxHeader
+          title="List of Porducts"
+          sideText={`${productData?.length} products`}
+        />
+        <Box
+          mt="0.5rem"
+          p="0 0.5rem"
+          height="75%"
+          sx={{
+            '& .MuiDataGrid-root': {
+              color: palette.grey[300],
+              border: 'none',
+            },
+            '& .MuiDataGrid-cell': {
+              borderBottom: `1px solid ${palette.grey[800]} !important`,
+            },
+            '& .MuiDataGrid-columnHeaders': {
+              borderBottom: `1px solid ${palette.grey[800]} !important`,
+            },
+            '& .MuiDataGrid-columnSeparator': {
+              visibility: 'hidden',
+            },
+          }}>
+          <DataGrid
+            columnHeaderHeight={25}
+            rowHeight={35}
+            hideFooter={true}
+            rows={productData || []}
+            columns={productColumns}
+          />
+        </Box>
+      </DashboardBox>
+      <DashboardBox gridArea="h">
+        <BoxHeader
+          title="Recent Orders"
+          sideText={`${transactionsData?.length} latest transactions`}
+        />
+        <Box
+          mt="1rem"
+          p="0 0.5rem"
+          height="80%"
+          sx={{
+            '& .MuiDataGrid-root': {
+              color: palette.grey[300],
+              border: 'none',
+            },
+            '& .MuiDataGrid-cell': {
+              borderBottom: `1px solid ${palette.grey[800]} !important`,
+            },
+            '& .MuiDataGrid-columnHeaders': {
+              borderBottom: `1px solid ${palette.grey[800]} !important`,
+            },
+            '& .MuiDataGrid-columnSeparator': {
+              visibility: 'hidden',
+            },
+          }}>
+          <DataGrid
+            columnHeaderHeight={25}
+            rowHeight={35}
+            hideFooter={true}
+            rows={transactionsData || []}
+            columns={transactionsColumns}
+          />
+        </Box>
+      </DashboardBox>
       <DashboardBox gridArea="i"></DashboardBox>
       <DashboardBox gridArea="j"></DashboardBox>
     </>
