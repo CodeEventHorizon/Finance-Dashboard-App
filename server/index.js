7ed789f5f9f4f268cc3dcf13c901854bee0f2d4b
CodeEventHorizon <nikoloz.9374@gmail.com>
2023-04-10 22:35:41 +0400

    add: transactions route in the server
    This commit adds a transactions route to the server

--- server/index.js
+++ server/index.js
@@ -7,9 +7,11 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import kpiRoutes from './routes/kpi.js';
 import productRoutes from './routes/product.js';
+import transactionRoutes from './routes/transaction.js';
+import Transaction from './models/Transaction.js';
 import Product from './models/Product.js';
 // import KPI from './models/KPI.js';
-// import { kpis, products } from './data/data.js';
+import { kpis, products, transactions } from './data/data.js';
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -25,25 +27,28 @@ app.use(cors());
 /* Routes */
 app.use('/kpi', kpiRoutes);
 app.use('/product', productRoutes);
+app.use('/transaction', transactionRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, async () => {
   console.log(`Server listening on: ${PORT}`);
   try {
-    await mongoose
-      .connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      /* ADD DATA ONE TIME ONLY */
-      // .then(async () => {
-      //   await mongoose.connection.db.dropDatabase();
-      //   KPI.insertMany(kpis);
-      // });
-      // .then(async () => {
-      //   Product.insertMany(products);
-      // });
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    /* ADD DATA ONE TIME ONLY */
+    // .then(async () => {
+    //   await mongoose.connection.db.dropDatabase();
+    //   KPI.insertMany(kpis);
+    // });
+    // .then(async () => {
+    //   Product.insertMany(products);
+    // });
+    // .then(async () => {
+    //   Transaction.insertMany(transactions);
+    // });
     console.log('Mongoose connected successfully');
   } catch (error) {
     console.error(error);
